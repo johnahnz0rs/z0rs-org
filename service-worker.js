c7c5af8c325a016fbccd31f4e4174cd28b9378f1
John Ahn <john@launchpartner.co>
2018-11-03 23:05:24 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/z0rs-org/precache-manifest.ef9c864bc4dbf0bb21b0633d1db7295f.js"
+  "/precache-manifest.f4d858e70fefa21aba4e7d9b4f9328a0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/z0rs-org/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
