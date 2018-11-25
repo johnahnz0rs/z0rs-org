c06240c95818a94382254775c3ba39ca5547ddbe
John Ahn <john@launchpartner.co>
2018-11-25 11:22:51 -0800

    rearranged portfolio; /build

---
+++ build/precache-manifest.10ccdf7957ff380687af70ee94f03b2f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "350529bc3a09f937868ec2c64738b6b0",
+    "url": "/static/media/dog_girl.350529bc.png"
+  },
+  {
+    "revision": "77b69caa16ab6ed6fa14",
+    "url": "/static/css/main.0dbbf851.chunk.css"
+  },
+  {
+    "revision": "58c1390298732b837c9c",
+    "url": "/static/js/1.58c13902.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01a54c49d6e84199aba1a83a52d93d45",
+    "url": "/static/media/instagram.01a54c49.png"
+  },
+  {
+    "revision": "fd7aa2e673da7183737b7cc108c0db7e",
+    "url": "/static/media/whatMusicTall.fd7aa2e6.png"
+  },
+  {
+    "revision": "515b9feb6ead3057c91e484b9c505f87",
+    "url": "/static/media/devjobs.515b9feb.png"
+  },
+  {
+    "revision": "1230cb1d6b83b7edb23d34618b25644c",
+    "url": "/static/media/showshuffle.1230cb1d.png"
+  },
+  {
+    "revision": "a7a7c65fe73bffd3255df0d16a29889c",
+    "url": "/static/media/rbp.a7a7c65f.png"
+  },
+  {
+    "revision": "e72ec5e151fe712a34346210feac387f",
+    "url": "/static/media/denileeart.e72ec5e1.png"
+  },
+  {
+    "revision": "77b69caa16ab6ed6fa14",
+    "url": "/static/js/main.77b69caa.chunk.js"
+  },
+  {
+    "revision": "e6715d46a460ddd9525021366efc5417",
+    "url": "/static/media/resume.e6715d46.png"
+  },
+  {
+    "revision": "4e10d894f2e21f78d5e81b393ff14dfd",
+    "url": "/static/media/phone.4e10d894.png"
+  },
+  {
+    "revision": "066441166cebbd939bfdc69787932297",
+    "url": "/static/media/text.06644116.png"
+  },
+  {
+    "revision": "47797a1e31109c31c918ba59f31ec7af",
+    "url": "/static/media/brand.47797a1e.png"
+  },
+  {
+    "revision": "726284cf900298df066ccee053d141a0",
+    "url": "/static/media/whatmusic.726284cf.png"
+  },
+  {
+    "revision": "a5b7c4e9e60ee67b27faf7edfddfb9ce",
+    "url": "/static/media/dog_truck.a5b7c4e9.png"
+  },
+  {
+    "revision": "9ca941a648b3f287f8db3cc46f266a82",
+    "url": "/static/media/dog_wink.9ca941a6.png"
+  },
+  {
+    "revision": "29250d366bb2334e4733aa574f527aab",
+    "url": "/static/media/dog_wrinkly.29250d36.png"
+  },
+  {
+    "revision": "193be5341fac65006c478d8230a97dc0",
+    "url": "/index.html"
+  }
+];
