6b0fd114d9c1bdebd2f0cab2e8a2272ecf0d15b7
John Ahn <john@launchpartner.co>
2018-12-15 20:46:41 -0800

    Updates

---
+++ precache-manifest.e210ce3690cab6162e3958b23cc23021.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6852d3399fe7b101fcf83b226865632",
+    "url": "/static/media/show-shuffle-leslie.e6852d33.png"
+  },
+  {
+    "revision": "b646897208c5b0a75341",
+    "url": "/static/css/main.ae77abfd.chunk.css"
+  },
+  {
+    "revision": "42493cf5663d1a241dcf",
+    "url": "/static/js/1.42493cf5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01a54c49d6e84199aba1a83a52d93d45",
+    "url": "/static/media/instagram.01a54c49.png"
+  },
+  {
+    "revision": "f940a1091fbb82cae9bf16d55ceb6597",
+    "url": "/static/media/resume-comingsoon.f940a109.png"
+  },
+  {
+    "revision": "fd7aa2e673da7183737b7cc108c0db7e",
+    "url": "/static/media/whatMusicTall.fd7aa2e6.png"
+  },
+  {
+    "revision": "515b9feb6ead3057c91e484b9c505f87",
+    "url": "/static/media/devjobs.515b9feb.png"
+  },
+  {
+    "revision": "b646897208c5b0a75341",
+    "url": "/static/js/main.b6468972.chunk.js"
+  },
+  {
+    "revision": "a7a7c65fe73bffd3255df0d16a29889c",
+    "url": "/static/media/rbp.a7a7c65f.png"
+  },
+  {
+    "revision": "e72ec5e151fe712a34346210feac387f",
+    "url": "/static/media/denileeart.e72ec5e1.png"
+  },
+  {
+    "revision": "350529bc3a09f937868ec2c64738b6b0",
+    "url": "/static/media/dog_girl.350529bc.png"
+  },
+  {
+    "revision": "96716ef2bccf4cc8c4a8c0bcf70594af",
+    "url": "/static/media/resume-webdev.96716ef2.png"
+  },
+  {
+    "revision": "7bc2f1a4b3d84a5de04764afa10dc6e7",
+    "url": "/static/media/resume-cook.7bc2f1a4.png"
+  },
+  {
+    "revision": "4e10d894f2e21f78d5e81b393ff14dfd",
+    "url": "/static/media/phone.4e10d894.png"
+  },
+  {
+    "revision": "47797a1e31109c31c918ba59f31ec7af",
+    "url": "/static/media/brand.47797a1e.png"
+  },
+  {
+    "revision": "69ee93fb26b6299a70c5d7cefc48d87e",
+    "url": "/index.html"
+  }
+];
